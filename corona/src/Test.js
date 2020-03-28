a3b8ba25e2302dbad76926edb8cfa9b2c5993ef5
blast3r <blast3r@freemail.hu>
2020-03-28 16:56:39 +0100

    add voice rcognition

--- corona/src/Test.js
+++ corona/src/Test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -7,36 +7,113 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+var SpeechRecognition = SpeechRecognition || window.webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent
+
+var colors = ['aqua', 'azure', 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = true;
+recognition.lang = 'en-US';
+recognition.interimResults = true;
+recognition.maxAlternatives = 1;
+
+var diagnostic = document.querySelector('.output');
+var bg = document.querySelector('html');
+var hints = document.querySelector('.hints');
+
+var colorHTML = '';
+colors.forEach(function (v, i, a) {
+    console.log(v, i);
+    colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
+});
+// hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.';
+
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
 }));
 
 function Test() {
-    const classes = useStyles();
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              News
+    const [listening, setListening] = useState(false);
+
+
+    function toggleListen() {
+        setListening(!listening);
+
+    }
+
+    useEffect(() => {
+        handleListen();
+    }, [listening]);
+
+
+    function onClick() {
+        toggleListen();
+    }
+
+    function handleListen() {
+        if (listening) recognition.start();
+
+
+        let finalTranscript = ''
+
+        recognition.onresult = event => {
+            let interimTranscript = ''
+
+            for (let i = event.resultIndex; i < event.results.length; i++) {
+                const transcript = event.results[i][0].transcript;
+                if (event.results[i].isFinal) finalTranscript += transcript + ' ';
+                else interimTranscript += transcript;
+            }
+            document.getElementById('interim').innerHTML = interimTranscript
+            document.getElementById('final').innerHTML = finalTranscript
+
+        }
+
+    }
+
+        const classes = useStyles();
+
+
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            News
             </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-}
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
 
-export default Test;
+                <div>
+                    <Button variant="contained" color="secondary" onClick={toggleListen}>
+                        Secondary
+                </Button>
+
+                    <div id='interim' ></div>
+                    <div id='final'></div>
+                </div>
+
+            </div>
+        );
+    }
+
+    export default Test;
