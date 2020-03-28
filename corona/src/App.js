14ed151bdc10f8682549a47ee42515b08b8f7638
blast3r <blast3r@freemail.hu>
2020-03-28 16:05:00 +0100

    add test navbar

--- corona/src/App.js
+++ corona/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Test} from './Test';
+import Test from './Test';
 import NavBar from './NavBar';
+import ButtonAppBar from './ButtonAppBar';
 
 function App() {
   return (
     <div>
-        <NavBar />
+       
+      <Test />
       </div>
   );
 }
